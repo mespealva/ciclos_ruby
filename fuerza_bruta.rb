c90e994eb455d85f4be05b038bf75c267d0e6136
maria alvarez <mar.alvareg@gmail.com>
2020-08-22 11:22:19 -0400

    bruerza bruta

--- fuerza_bruta.rb
+++ fuerza_bruta.rb
@@ -1,22 +1,12 @@
 pass= ARGV[0]
-#    a = ('a'..'z').to_a
-#    b = pass.chars   
-#    for i in 0..(b.length) do
-#        for j in 0..(a.length) do
-#            if a[j]==b[i]
-#            cont+=1+j
-#            end
-#        end
-#    end
-
-#me da uno de mas, arreglar
-cont=0  
-clave="a"
-loop do
-    cont += 1
-    if clave==pass
-        break
+    a = ('a'..'z').to_a
+    b = pass.chars   
+    for i in 0..(b.length) do
+        for j in 0..(a.length) do
+            if a[j]==b[i]
+            cont+=1+j
+            end
+        end
     end
-    clave.next!
-end
-puts cont
+
+
