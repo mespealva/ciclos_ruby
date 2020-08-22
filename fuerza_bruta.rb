30ae01bd98749f601f43c22b7e97c89142bdcbe6
maria alvarez <mar.alvareg@gmail.com>
2020-08-22 16:53:24 -0400

    fuerza_bruta al fin

--- fuerza_bruta.rb
+++ fuerza_bruta.rb
@@ -1,16 +1,10 @@
 pass= ARGV[0]
-    a = ('a'..'z').to_a
-    b = pass.chars   
-    cont=0
-    for i in 0..(b.length) do
-        for j in 0..(a.length) do
-            if a[j]==b[i]
-            cont+=1+j
-            else
-                cont+=1+i
-            end
 
-        end
-    end
+a = "a"
+cont = 0
+while a != pass do
+    cont += 1
+    a.next!
+end
 
 puts cont
